c5861cb2d92e1fe9c5b7723623dc253d522c3872
coachvredin <43240053+coachvredin@users.noreply.github.com>
2021-03-08 15:34:14 +0100

    Put the code into VS

---
+++ Source/TrainEngine/TrainPlanner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine
+{
+	interface ITrainPlanner
+	{
+		ITrainPlanner StartTrainAt(string startTrain);
+		ITrainPlanner StopTrainAt(string stopTrain);
+		TravelPlan ToPlan();
+	}
+
+	class TrainPlanner
+    {
+		ITrainPlanner StartTrainAt(string startTrain);
+		ITrainPlanner StopTrainAt(string stopTrain);
+		TravelPlan ToPlan();
+	}
+
+	class TravelPlan
+	{
+
+	}
+
+	class Train
+	{
+		int Id;
+		string Name;
+		int MaxSpeed;
+		bool Operated;
+	}
+}
