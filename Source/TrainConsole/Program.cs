8788d271d4683c1f4a70bfdccc7107816d224a95
JoCh <johancz@users.noreply.github.com>
2021-03-09 17:00:45 +0100

    Created a class which reads and parses the schedule class
    ## Also done:
    
    ### In "Program.cs":
    - Loop through each train-row which we've imported from "trains.txt" and create a "Train" object.
    - Using the data imported from the file "timetable.txt", create a "Schedule" object for each train.
    - Use the "Train" and "Schedule" objects to create a "TravelPlan" object.
    
    ### In "TrainPlanner.cs":
    - Update the Schedule class so that it contains a trainId and a "List<TimetableStop>, and add a constructor which takes these items.
    - Add a class called "TimetableStop" which is very similar to the "Stop"-class, the difference being that the new class takes a (int)"TrainId" (int)"StationId" instead of a "Station"-object, and has two "time" Properties for departure and arrival instead of a single generic "time" property.
    - Finish the Train-constructor.

--- Source/TrainConsole/Program.cs
+++ Source/TrainConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using TrainConsole;
 using TrainEngine;
 
@@ -8,6 +10,53 @@ namespace TrainConsole
     {
         static void Main(string[] args)
         {
+            ScheduleDataFile scheduleFile = new ScheduleDataFile("Data/timetable.txt");
+            FileData passengerFile = new FileData("Data/passengers.txt", ';');
+            FileData stationFile = new FileData("Data/stations.txt", '|');
+            FileData trainFile = new FileData("Data/trains.txt", ',');
+
+            if (trainFile.FileLines.Count > 1)
+            {
+                for (int y = 1; y < trainFile.FileLines.Count; y++)
+                {
+                    string[] trainData = trainFile.FileLines[y];
+                    int id;
+                    int maxSpeed;
+                    bool operated;
+
+                    // Note: Should we move the code below which parses the train rows, to its own class (i.e. "TrainsDataFile")? Like we do with the "Data/timetable.txt" file in the "ScheduleDataFile"-class?
+                    try
+                    {
+                        id = int.Parse(trainData[0]);
+                        maxSpeed = int.Parse(trainData[2]);
+                        operated = bool.Parse(trainData[3]);
+
+                        Train newTrain = new Train(id, trainData[1], maxSpeed, operated);
+
+                        // Create the schedule for the train "newTrain".
+                        List<TimetableStop> scheduleStops = scheduleFile.Stops.Where(stop => stop.TrainId == newTrain.Id).ToList();
+                        Schedule newSchedule = new Schedule(newTrain.Id, scheduleStops);
+
+                        // Create the travel plan for the train "newTrain".
+                        ITravelPlan travelPlan3 = new TrainPlanner(newTrain)
+                                .ReadSchedule(newSchedule)
+                                //.LevelCrossing()
+                                //.CloseAt("10:23")
+                                //.OpenAt("10:25")
+                                //.SetSwitch(switch1, SwitchDirection.Left)
+                                //.SetSwitch(switch2, SwitchDirection.Right)
+                                .GeneratePlan();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+
+            /* --- Old code --------------------------------------------- */
+
             // Old Stuff:
             //Console.WriteLine("Train track!");
             // Step 1:
@@ -19,37 +68,29 @@ namespace TrainConsole
 
             //ITrainPlanner newPlanner = new TrainPlanner().StartTrainAt("10:23").StopTrainAt("15:09").ToPlan();
 
-            FileData scheduleFile = new FileData("Data/timetable.txt", ',');
-            //FileData passengerFile = new FileData("passengers.txt", ';');
-            //FileData stationFile = new FileData("stations.txt", '|');
-            //FileData trainFile = new FileData("trains.txt", ',');
 
-            // New stuff:
-            Train train1 = new Train("Name of train");
-            Station station1 = new Station("Gothenburg");
-            Station station2 = new Station("Stockholm");
-            Schedule schedule1 = new Schedule
-            {
 
-            };
+            //Train train1 = new Train(1, "Flying Scotsman", 100, false);
+            //Station station1 = new Station("Gothenburg");
+            //Station station2 = new Station("Stockholm");
 
-            ITravelPlan travelPlan = new TrainPlanner(train1, station1)
-                      //.HeadTowards(station2)
-                      //.ReadSchedule()
-                      .StartTrainAt("10:23", true)
-                      .StopTrainAt(station2, "14:53")
-                      .GeneratePlan();
+            //ITravelPlan travelPlan = new TrainPlanner(train1, station1)
+            //          //.HeadTowards(station2)
+            //          //.ReadSchedule()
+            //          .StartTrainAt("10:23", true)
+            //          .StopTrainAt(station2, "14:53")
+            //          .GeneratePlan();
 
-            foreach (Stop s in travelPlan.Stops)
-            {
-                Console.WriteLine($"{s.Station.Name} {s.Time}");
-            }
+            //foreach (Stop s in travelPlan.Stops)
+            //{
+            //    Console.WriteLine($"{s.Station.Name} {s.Time}");
+            //}
 
-            Console.WriteLine(scheduleFile.FileLines[0][2]);
+            //Console.WriteLine(scheduleFile.FileLines[0][2]);
 
-            Console.ReadLine();
+            //Console.ReadLine();
 
-            //travelPlan.Start();
+            ////travelPlan.Start();
         }
     }
 }
--- Source/TrainEngine/TrainPlanner.cs
+++ Source/TrainEngine/TrainPlanner.cs
@@ -6,146 +6,203 @@ using System.IO;
 
 namespace TrainEngine
 {
-	public interface ITrainPlanner
-	{
-		ITrainPlanner StartTrainAt(string startTrain, bool DirectionForward);
-		ITrainPlanner StopTrainAt(Station station, string stopTrain);
-		ITravelPlan GeneratePlan();
-		ITrainPlanner ReadSchedule(Schedule schedule);
-    }
-	
-	public class FileData
+    public interface ITrainPlanner
     {
-		public string FileName;
-		public List<string[]> FileLines { get; } = new List<string[]>();
-
-		public FileData(string filePath, char separator)
-		{
-			string[] lines = File.ReadAllLines(filePath);
-			foreach (string line in lines)
-			{
-				string[] columns = line.Split(separator).Select(a => a.Trim()).ToArray();
-				FileLines.Add(columns);
-			}
-		}
+        ITrainPlanner StartTrainAt(string startTrain, bool DirectionForward);
+        ITrainPlanner StopTrainAt(Station station, string stopTrain);
+        ITravelPlan GeneratePlan();
+        ITrainPlanner ReadSchedule(Schedule schedule);
     }
 
-	public class Schedule
+    // Note: split into a separate class for each data file type? ("train.txt", "passangers.txt" ...etc).
+    public class FileData
     {
-		public int StationID;
-		public string StationName;
-		public int ArrivalTime;
-		public int DepartureTime;
-		public bool DirectionForward;
-    }
+        public string FileName;
+        public List<string[]> FileLines { get; } = new List<string[]>();
 
-	public class TrainPlanner : ITrainPlanner 
-    {
-		public ITrainPlanner ReadSchedule(Schedule schedule)
+        public FileData(string filePath, char separator)
         {
-			return this;
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(separator).Select(a => a.Trim()).ToArray();
+                FileLines.Add(columns);
+            }
+        }
+    }
+
+    public class ScheduleDataFile
+    {
+        public List<TimetableStop> Stops { get; } = new List<TimetableStop>();
+
+        public ScheduleDataFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(',').Select(a => a.Trim()).ToArray();
+
+                // Parse the first two columns to ints.
+                int trainId = int.Parse(columns[0]);
+                int stationId = int.Parse(columns[1]);
+
+                // Parse the 3rd column to time in seconds.
+                int[] tempDepartureTime = columns[2].Split(':').Select(a => int.Parse(a)).ToArray();
+                int departureTime = tempDepartureTime[0] * 360 + tempDepartureTime[1] * 60;
+
+                // Parse the 4th column to time in seconds.
+                int[] tempArrivalTime = columns[3].Split(':').Select(a => int.Parse(a)).ToArray();
+                int arrivalTime = tempArrivalTime[0] * 360 + tempArrivalTime[1] * 60;
+
+                Stops.Add(new TimetableStop(trainId, stationId, departureTime, arrivalTime));
+            }
+        }
+    }
+
+    public class Schedule
+    {
+        public int TrainId { get; set; }
+        public List<TimetableStop> Stops { get; set; } = new List<TimetableStop>();
+        public bool DirectionForward;
+
+        public Schedule(int trainId, List<TimetableStop> stops)
+        {
+            Stops = stops;
         }
 
-		private List<Stop> _stops = new List<Stop>();
+    }
 
+    public class TrainPlanner : ITrainPlanner
+    {
+
+        //private List<Stop> _stops = new List<Stop>();
+        private List<TimetableStop> _stops = new List<TimetableStop>();
+        private Train _train;
         public bool DirectionForward = true;
 
-		public TrainPlanner(Train train, Station station)
+        //public TrainPlanner(Train train, Station station)
+        //{
+        //    _stops.Add(new Stop(station));
+        //}
+
+        public TrainPlanner(Train train)
         {
-            _stops.Add(new Stop(station));
+            _train = train;
         }
 
-		public ITrainPlanner StartTrainAt(string startTrain, bool directionForward)
+        public ITrainPlanner ReadSchedule(Schedule schedule)
+        {
+            _stops = schedule.Stops;
+            return this;
+        }
+
+        public ITrainPlanner StartTrainAt(string startTrain, bool directionForward)
         {
             DirectionForward = directionForward;
 
-			int[] time = startTrain.Split(':').Select(a => int.Parse(a)).ToArray();
-			int startTime = time[0] * 60 + time[1];
+            int[] time = startTrain.Split(':').Select(a => int.Parse(a)).ToArray();
+            int startTime = time[0] * 60 + time[1];
 
-			_stops[0].Time = startTime;
+            _stops[0].Time = startTime;
 
             return this;
         }
 
-		public ITrainPlanner StopTrainAt(Station station, string stopTrain)
+        public ITrainPlanner StopTrainAt(Station station, string stopTrain)
         {
-			int[] time = stopTrain.Split(':').Select(a => int.Parse(a)).ToArray();
-			int stopTime = time[0] * 60 + time[1];
+            int[] time = stopTrain.Split(':').Select(a => int.Parse(a)).ToArray();
+            int stopTime = time[0] * 60 + time[1];
 
-			_stops.Add(new Stop(station, stopTime));
+            _stops.Add(new Stop(station, stopTime));
 
-			return this;
-		}
+            return this;
+        }
 
         public ITravelPlan GeneratePlan()
         {
-			return new TravelPlan(_stops);
+            return new TravelPlan(_stops);
         }
-
-		
-		
     }
 
-	public interface ITravelPlan
+    public interface ITravelPlan
     {
         public List<Stop> Stops { get; set; }
 
-	}
+    }
 
-	public class TravelPlan : ITravelPlan
+    public class TravelPlan : ITravelPlan
     {
         public List<Stop> Stops { get; set; }
 
-		public void Start()
+        public void Start()
         {
 
         }
 
-		public TravelPlan(List<Stop> stops)
+        public TravelPlan(List<Stop> stops)
         {
             Stops = stops;
         }
-	}
-
-	public class Stop
-    {
-		public Station Station { get; set; }
-		public int Time { get; set; }
-
-		public Stop(Station station)
-		{
-			Station = station;
-		}
-
-		public Stop(Station station, int time)
-		{
-			Station = station;
-			Time = time;
-		}
     }
 
-	public class Train
-	{
-		private int _id;
-		private string _name;
-		private int _maxSpeed;
-		private bool _operated;
+    public class Stop
+    {
+        public Station Station { get; set; }
+        public int Time { get; set; }
 
-		public Train(string name)
+        public Stop(Station station)
         {
-			_name = name;
+            Station = station;
         }
-}
 
-	public class Station
-	{
-		// private int _id;
-		public string Name { get; }
-		// private bool _endStation;
+        public Stop(Station station, int time)
+        {
+            Station = station;
+            Time = time;
+        }
+    }
 
-		public Station(string name)
+    public class TimetableStop
+    {
+        public int TrainId { get; set; }
+        public int StationId { get; set; }
+        public int DepartureTime { get; set; } // time in seconds
+        public int ArrivalTime { get; set; } // time in seconds
+
+        public TimetableStop(int trainId, int stationId, int departureTime, int arrivalTime)
+        {
+            TrainId = trainId;
+            StationId = stationId;
+            DepartureTime = departureTime;
+            ArrivalTime = arrivalTime;
+        }
+    }
+
+    public class Train
+    {
+        public int Id;
+        public string Name;
+        public int MaxSpeed;
+        public bool Operated;
+
+        public Train(int id, string name, int maxSpeed, bool operated)
+        {
+            Id = id;
+            Name = name;
+            MaxSpeed = maxSpeed;
+            Operated = operated;
+        }
+    }
+
+    public class Station
+    {
+        // private int _id;
+        public string Name { get; }
+        // private bool _endStation;
+
+        public Station(string name)
         {
             Name = name;
         }
-	}
+    }
 }
